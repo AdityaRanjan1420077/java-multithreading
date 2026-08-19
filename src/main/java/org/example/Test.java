a96a61bad04f16cc39814db6914a53a3a80be232
AdityaRanjan1420077 <aditya.ranjan13658683@gmail.com>
2026-08-19 10:51:25 +0530

    Add main thread

---
+++ src/main/java/org/example/Test.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Test {
+    public static void main(String[] args) {
+//        System.out.println();
+        System.out.println("Hello World");
+        System.out.println("The name of the this thread is " + Thread.currentThread().getName());
+    }
+
+}
