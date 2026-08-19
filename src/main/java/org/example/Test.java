a262471d8c5116a476e440b9b0c29c09bcc1e524
AdityaRanjan1420077 <aditya.ranjan13658683@gmail.com>
2026-08-19 11:13:50 +0530

    Ways to implement threads- 1. Implements the Runnable Interface 2.Extending the thread class

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -5,6 +5,48 @@ public class Test {
 //        System.out.println();
         System.out.println("Hello World");
         System.out.println("The name of the this thread is " + Thread.currentThread().getName());
+//        for(int i = 0; i < 1000; i++){
+//            System.out.println("Hello");
+//        }
+
+
+        //I have to run the thread present in the World class
+//        1. Create the object of the World class
+//        2. call the start method of the object
+        World world = new World();
+        world.start();
+
+
+//        System.out.println("Infinite Loop");
+//        //Infinite Looping
+////        for(; ;) {
+////            System.out.println("Hello");
+////        }
+//
+//        for(; ;) {
+//            System.out.println(Thread.currentThread().getName());
+//        }
+        //By Implementing the Runnable Interface
+//    1. Create a new Object of the class
+//    2.Create a new thread
+//    3. Pass the name of object in that thread constructor
+//    4. t1.start();
+
+        System.out.println("Creating the object of the Car class");
+        Car car = new Car();
+        System.out.println("Creating a new thread t1 and passing car into that constructor");
+        Thread t1 = new Thread(car);
+        System.out.println("Start the thread");
+        t1.start();
+
+
+
+
+
     }
+
+
+
+
 
 }
